9cd138bc8a22a8e45eaa47a0c891f91afbdadfa9
Benz <benzhomeme@gmail.com>
2023-08-03 10:40:34 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <title> Benz Resume </title>
+    <h1 className="text-15xl font-bold ">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
